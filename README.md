3899443ed82cb2b7ba2cf34b311c2db54caece4d
Pump4Love <33705945+pump4love@users.noreply.github.com>
2023-12-09 22:43:47 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# RoundEndLowGravity
-RoundEndLowGravity
+# CS2-RoundEndLowGravity
+
+RoundEndLowGravity.
+
+### Requirements
+[CounterStrikeSharp](https://github.com/roflmuffin/CounterStrikeSharp/releases/)
+
+### Installation
+
+Drag and drop from [releases](https://github.com/Franc1sco/CS2-RoundEndLowGravity/releases) to game/csgo/addons/counterstrikesharp/plugins
---
+++ RoundEndLowGravity.cs
@@ -0,0 +1,32 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace RoundEndLowGravity;
+
+public class RoundEndLowGravity : BasePlugin
+{
+    public override string ModuleName => "RoundEndLowGravity";
+
+    public override string ModuleVersion => "0.0.2";
+
+    public override string ModuleAuthor => "Pump4Love";
+
+    public override string ModuleDescription => "RoundEndLowGravity.";
+
+    public override void Load(bool hotReload)
+    {
+        RegisterEventHandler<EventRoundEnd>(Event_RoundEnd, HookMode.Post);
+    }
+
+    int endtimer = ConVar.Find("sv_cheats");
+
+    private HookResult Event_RoundEnd(EventRoundEnd @event, GameEventInfo info)
+    {
+        NativeAPI.IssueServerCommand("sv_gravity 200");
+
+        base.AddTimer(endtimer, () =>
+        {
+            NativeAPI.IssueServerCommand("sv_gravity 800");
+        });
+        return HookResult.Continue;
+    }
+}
